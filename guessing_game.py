08dcde32595a951f6f4e693db0c5f426ac9b06bb
Keith Frazier <keithfrazier98@gmail.com>
2020-07-01 23:03:35 -0700

    Return number of guesses added
    count of guesses added
    
    the raise_count funciton raises a count of guesses
    
    start_game() returns the score
    
    the 1st start_game() call is set = to score

--- guessing_game.py
+++ guessing_game.py
@@ -6,23 +6,32 @@ def start_game():
 Welcome To The Number Guessing Game! 
 ------------------------------------ 
                                     """)
-    
+    guess_count = 0
     def question():
         return int(input("Pick a number between 1 and 10  "))
+    def raise_count(a_count):
+        return a_count + 1
     a_guess = question()
-    guess_count = 1
+    guess_count = raise_count(guess_count)
     random_num = random.randint(1,10)
     
     while a_guess != random_num:
+        
+        if  a_guess > 10:
+            print("Oops! Try Again.")
+            guess_count = raise_count(guess_count)
+            a_guess = question()
         if a_guess < random_num: 
-            print("It's higher!") 
+            print("It's higher!")
+            guess_count = raise_count(guess_count)
             a_guess = question()
         elif a_guess > random_num:
             print("It's lower!")
+            guess_count = raise_count(guess_count)
             a_guess = question()
-            continue
         else:
-            print("You got it! It took you {} tries".format)
             break
-    
-start_game()
+    return guess_count
+score = start_game()
+
+print("You got it! It took you {} tries".format(score))
