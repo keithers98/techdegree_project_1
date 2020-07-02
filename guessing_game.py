93f09e48f23e788f78da37a727d48a41a4474ac5
Keith Frazier <keithfrazier98@gmail.com>
2020-07-01 21:06:18 -0700

    Header code and first promt

---
+++ guessing_game.py
@@ -0,0 +1,15 @@
+import random
+
+def start_game():
+    print("""
+------------------------------------
+Welcome To The Number Guessing Game! 
+------------------------------------ 
+                                    """)
+    first_guess = input("Pick a number between 1 and 10")
+    
+
+    
+    
+    
+start_game()
