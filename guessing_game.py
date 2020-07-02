edade905cc665242a4447df3d6377171dbb520a4
Keith Frazier <keithfrazier98@gmail.com>
2020-07-01 22:08:43 -0700

    Added while loop  and question function
    Im unsure if this was a redundant thing to do, but i created a function for the question. I at least don't have to type the question out over and over.

--- guessing_game.py
+++ guessing_game.py
@@ -6,10 +6,23 @@ def start_game():
 Welcome To The Number Guessing Game! 
 ------------------------------------ 
                                     """)
-    first_guess = input("Pick a number between 1 and 10")
     
-
+    def question():
+        return int(input("Pick a number between 1 and 10  "))
+    a_guess = question()
+    guess_count = 1
+    random_num = random.randint(1,10)
     
-    
+    while a_guess != random_num:
+        if a_guess < random_num: 
+            print("It's higher!") 
+            a_guess = question()
+        elif a_guess > random_num:
+            print("It's lower!")
+            a_guess = question()
+            continue
+        else:
+            print("You got it! It took you {} tries".format)
+            break
     
 start_game()
